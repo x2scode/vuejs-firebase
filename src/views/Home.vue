02d0b7ac8866e89af025879dcfde7e87e7a8f62f
x2scode <x2scode@yahoo.com>
2021-01-22 15:57:18 -0800

    update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="home">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/admin">Admin</router-link>
+<div class="uk-container-cover">
+
+    <div class="uk-background-cover uk-height-medium uk-panel image-background" uk-height-viewport>
+
+        <div class="uk-flex uk-flex-center uk-margin-top">
+          <div class="uk-alert-warning" uk-alert>
+              <p>Due to the use of firebase, you must use a proxy change</p>
+          </div>
+        </div>
+        <div class="uk-flex uk-flex-center uk-margin-small">
+            <router-link class="uk-button uk-button-primary uk-margin-right" to="/">Home</router-link> 
+            <router-link class="uk-button uk-button-secondary" to="/admin">Admin</router-link>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -13,3 +24,8 @@ export default {
   
 }
 </script>
+<style scoped>
+.image-background{
+  background-image: url('../../src/assets/blog-image.jpg');
+}
+</style>
